d3179cc65e462347932e718f945793676b171fb9
WillyXJ <WillyXJ@users.noreply.github.com>
2026-09-04 07:49:12 -0600

    version bump

--- client/facileManager/fmDNS/client.php
+++ client/facileManager/fmDNS/client.php
@@ -29,7 +29,7 @@
  */
 
 /** Client version */
-$data['server_client_version'] = '7.4.4';
+$data['server_client_version'] = '7.4.5';
 
 error_reporting(0);
 
--- server/fm-modules/fmDNS/variables.inc.php
+++ server/fm-modules/fmDNS/variables.inc.php
@@ -31,8 +31,8 @@ if (!@is_array($__FM_CONFIG)) $__FM_CONFIG = array();
 
 /** Module Information */
 $__FM_CONFIG['fmDNS'] = array(
-		'version'							=> '7.4.4',
-		'client_version'					=> '7.4.4',
+		'version'							=> '7.4.5',
+		'client_version'					=> '7.4.5',
 		'description'						=> __('Easily manage one or more ISC BIND servers through a web interface. No more editing configuration and zone files manually.', 'fmDNS'),
 		'prefix'							=> 'dns_',
 		'required_dns_version'				=> '9.3',
